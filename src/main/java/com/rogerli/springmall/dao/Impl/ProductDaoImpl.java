3450cef9f076a73fb85c00ff11f37a0458103f1a
mingyao <mingyao135@gmail.com>
2023-04-18 22:06:31 +0800

    Feat : 新增單元測試

--- src/main/java/com/rogerli/springmall/dao/Impl/ProductDaoImpl.java
+++ src/main/java/com/rogerli/springmall/dao/Impl/ProductDaoImpl.java
@@ -31,14 +31,7 @@ public class ProductDaoImpl implements ProductDao {
                 "FROM product " +
                 "WHERE 1=1 ";
         Map<String, Object> map = new HashMap<>();
-        if (productQueryParams.getCategory() != null){
-            sql = sql + " AND category = :category ";
-            map.put("category",productQueryParams.getCategory().name());
-        }
-        if (productQueryParams.getSearch() != null) {
-            sql = sql + " AND product_name LIKE :search ";
-            map.put("search", "%" + productQueryParams.getSearch() + "%");
-        }
+        sql = addFilteringSql(sql,map,productQueryParams);
         //排序
         sql = sql + " ORDER BY " + productQueryParams.getOrderBy() + " " + productQueryParams.getSort();
         //分頁
@@ -119,6 +112,13 @@ public class ProductDaoImpl implements ProductDao {
     public Integer countProduct(ProductQueryParams productQueryParams) {
         String sql = "SELECT count(*) FROM product WHERE 1=1 ";
         Map<String, Object> map = new HashMap<>();
+        addFilteringSql(sql,map,productQueryParams);
+
+        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
+        return total;
+    }
+
+    private String addFilteringSql(String sql, Map<String,Object> map, ProductQueryParams productQueryParams){
         if (productQueryParams.getCategory() != null){
             sql = sql + " AND category = :category ";
             map.put("category",productQueryParams.getCategory().name());
@@ -127,10 +127,7 @@ public class ProductDaoImpl implements ProductDao {
             sql = sql + " AND product_name LIKE :search ";
             map.put("search", "%" + productQueryParams.getSearch() + "%");
         }
-
-        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
-        return total;
+        return sql;
     }
-
 
 }
