c4ecb834bb0098bb816d6a973948f2d69eba0cf5
mingyao <mingyao135@gmail.com>
2023-04-23 16:29:44 +0800

    Feat : 新增檢查email是否已註冊、隱藏返回的密碼功能

--- src/main/java/com/rogerli/springmall/dao/Impl/UsreDaoImpl.java
+++ src/main/java/com/rogerli/springmall/dao/Impl/UsreDaoImpl.java
@@ -53,4 +53,18 @@ public class UsreDaoImpl implements UserDao {
         int userId = keyHolder.getKey().intValue();
         return userId;
     }
+
+    @Override
+    public User getUserByEmail(String email) {
+        String sql = "SELECT user_id, email, password, created_date, last_modified_date " +
+                "From user WHERE email = :email";
+        Map<String, Object> map = new HashMap<>();
+        map.put("email", email);
+        List<User> userList = namedParameterJdbcTemplate.query(sql,map,new UserRowMapper());
+        if (userList.size()>0){
+            return userList.get(0);
+        }else {
+            return null;
+        }
+    }
 }
--- src/main/java/com/rogerli/springmall/service/Impl/UserServiceImpl.java
+++ src/main/java/com/rogerli/springmall/service/Impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import com.rogerli.springmall.dao.UserDao;
 import com.rogerli.springmall.dto.UserRegisterRequest;
 import com.rogerli.springmall.model.User;
 import com.rogerli.springmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
@@ -19,6 +25,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null){
+            log.error("該 email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
 
         return userDao.createUser(userRegisterRequest);
     }
